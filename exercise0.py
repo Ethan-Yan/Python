4e66038c4cd4e57ed3ba4080f3172d64c68cdaa4
Ethan-Yan <yanzhiliang2013@gmail.com>
2016-03-27 15:27:29 +0800

    update

--- exercise0.py
+++ exercise0.py
@@ -1,9 +1,15 @@
 # Strings
 # s = '<any string>'
-s = "Hello! I'm Ethan."
+
+# <string>[<expression>] --> one-character string
+
+s = "Hello"
 s[3], s[1 + 1 + 1]
 s[0], (s + s)[0]
 s[0] + s[1], s[0 + 1]
 s[1], (s + 'ity')[1]
-
 s[-1], (s + s)[-1]
+
+# <string>[<expression>:<expression>] --> subsequence
+
+s[0:4]
