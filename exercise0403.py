7395dc8e3cd8a3a3b81456c08980798b37d15a0c
Ethan-Yan <yanzhiliang2013@gmail.com>
2016-04-03 15:48:29 +0800

    Update

--- exercise0403.py
+++ exercise0403.py
@@ -1,4 +1,3 @@
-'''
 
 def decor(func):
     def wrap():
@@ -37,7 +36,7 @@ def is_odd(x):
 print(is_odd(17))
 print(is_even(23))
 
-'''
+
 # Sets
 
 num_set = {1, 2, 3, 4, 5}
@@ -45,3 +44,21 @@ word_set = set(['spam', 'eggs', 'sausage'])
 
 print(3 in num_set)
 print('spam' not in word_set)
+
+
+nums = {1, 2, 3, 4, 5, 6}
+print(nums)
+nums.add(-7)
+nums.remove(3)
+print(nums)
+
+
+first = {1, 2, 3, 4, 5, 6}
+second = {4, 5, 6, 7, 8, 9}
+
+print(first | second)
+print(first & second)
+print(first - second)
+print(second - first)
+print(first ^ second)
+
